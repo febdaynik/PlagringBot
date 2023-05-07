e137d15fa4a201592288ffa6395a4efb6c398b4c
febdaynik <drons_dron@mail.ru>
2023-05-08 02:50:24 +0300

    small fix

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -4,6 +4,6 @@ from aiogram import types
 async def game_callback(call: types.CallbackQuery):
 
 	# if call.game_short_name is not None:
-	await call.answer(url='https://febdaynik.github.io/PlagringBot')
+	await call.answer(url='https://febdaynik.github.io/PlagringBot/games/tic_tac_toe')
 
 	# await call.answer()
